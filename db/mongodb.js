ee410b0680324a53101e9bd9684bded4348bc1c2
MeatPieYan <261893666@qq.com>
2016-01-04 23:04:40 +0800

    update mongo ip

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 
 console.info("========== Robin INFO || DB connect ==========");
-mongoose.connect('mongodb://bbf9ada967f34a8d8b1e3aa9f28305a5:e585cffe1f6d470fb42f1d5475294fa1@mongo.duapp.com:8908/fnfzWBVrxPGREFTuAlKw', {server: {auto_reconnect:true}});
+mongoose.connect('mongodb://127.0.0.1:27017/Robin', {server: {auto_reconnect:true}});
 
 mongoose.connection.on('close', function(){
 	console.info("========== Robin INFO || DB re-connect ==========");
-	mongoose.connect('mongodb://bbf9ada967f34a8d8b1e3aa9f28305a5:e585cffe1f6d470fb42f1d5475294fa1@mongo.duapp.com:8908/fnfzWBVrxPGREFTuAlKw', {server:{auto_reconnect:true}});
+	mongoose.connect('mongodb://127.0.0.1:27017/Robin', {server:{auto_reconnect:true}});
 });
 
 mongoose.connection.on('error', function(error){
